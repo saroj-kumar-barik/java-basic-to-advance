2257f9197699de6b8dc673a7971e14aba2997518
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2020-12-26 00:05:04 +0530

    added hirerchial inheritance

---
+++ src/inheritance/Hirerchial_Inhert.java
@@ -0,0 +1,36 @@
+package inheritance;
+
+class Onee
+{
+    public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+}
+
+class Twoo extends Onee
+{
+    public void print_for()
+    {
+        System.out.println("for");
+    }
+
+}
+
+class Threee extends Onee
+{
+    /*............*/
+}
+
+
+public class Hirerchial_Inhert
+{
+    public static void main(String[] args)
+    {
+        Threee g = new Threee();
+        g.print_geek();
+        Twoo t = new Twoo();
+        t.print_for();
+        g.print_geek();
+    }
+} 
