6c740718124d613e371bffb4a9269dab69475e7e
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2021-02-10 22:29:13 +0530

    quiz on exception handling

---
+++ src/ExceptionHandling/Quiz2.java
@@ -0,0 +1,18 @@
+package ExceptionHandling;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+        try {
+            badMethod();
+            System.out.println("a");
+        } catch (Throwable e) {
+            System.out.println("Exc");
+        } finally {
+            System.out.println("finally");
+        }
+        System.out.println("another normal srat");
+    }
+        public static void badMethod(){
+            throw new Error();
+        }
+    }
